6d2a6c2b2b13cc70c8af3169c962e9fc665a03b0
Webia1 <eb@ebia.eu>
2018-01-21 19:06:23 +0100

    Component from vue-property-decorator

--- src/app/App.vue
+++ src/app/App.vue
@@ -8,8 +8,7 @@
 <script>
 
     import Vue from 'vue';
-    import Component from 'vue-class-component';
-    import {Prop} from 'vue-property-decorator';
+    import {Component, Prop} from 'vue-property-decorator';
 
     @Component({})
     export default class App extends Vue {
