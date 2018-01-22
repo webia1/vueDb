56de7d48ee22156f5ed7d7f84db47784e6af835d
Webia1 <erkan.buelbuel@gmail.com>
2018-01-22 10:06:54 +0100

    The same now dynamically

--- src/settings/SomeSettings.vue
+++ src/settings/SomeSettings.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        <slot header="Some Settings Header"></slot>
+        <slot :header="header" :footer="footer"/>
     </div>
 </template>
+
+<script>
+    import Vue from 'vue';
+    import Component from 'vue-class-component';
+
+    Component ({})
+    export default class SomeSettings extends Vue {
+        header = "Some Settings Header";
+        footer = "Some Settings Footer"
+    }
+</script>
